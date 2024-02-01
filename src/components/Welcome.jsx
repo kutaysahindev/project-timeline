297f534e273f814b3f4de06484bd962017aaacf4
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-02-01 21:18:01 +0300

    Major styling changes & Add footer component
    https://youtube.com/shorts/QFbSXb-6YDk?si=ybneQ4UyLaF6usjO

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,15 @@
+import { FaGithub } from "react-icons/fa";
+
 const Welcome = () => {
   return (
     <div className="p-40 flex flex-col items-center gap-5">
       <h1 className="text-7xl font-bold">Arçelik AI Platform</h1>
-      <h6 className="text-rose-600 font-">Arçelik Global</h6>
-      <button>Go to platform</button>
+      <h6 className="text-gray-600 font-semibold">Project Showcase</h6>
+      <button className="flex items-center gap-2 bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">
+        <FaGithub />
+        View on GitHub
+      </button>
     </div>
-  )
-}
-export default Welcome
+  );
+};
+export default Welcome;
