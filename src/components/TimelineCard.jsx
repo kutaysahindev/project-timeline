7990111753f1e727e2bd99d30b44308f38be6e0d
Hasan Talha Çelik <35665454+bitterkofte@users.noreply.github.com>
2024-02-01 17:27:55 +0300

    lining the content according to the timeline

--- src/components/TimelineCard.jsx
+++ src/components/TimelineCard.jsx
@@ -58,20 +58,20 @@ const TimelineCard = ({ id, tagline, title, description }) => {
   
 
   return (
-    <div className='min-h-80 mb-10 flex gap-10 ' ref={cardRef}>
-      <div className="p-5 flex flex-col items-center gap-5  ">
-        <h1 className={`pt-5 text-4xl font-extrabold transition-all duration-400 ${enlight ? "text-gray-100" : "text-gray-600"}`}>
+    <div className='min-h-80 flex gap-10 ' ref={cardRef}>
+      <div className="px-5 flex flex-col items-center gap-5  ">
+        <h1 className={`pt-3 text-6xl font-extrabold transition-all duration-400 ${enlight ? "text-gray-100" : "text-gray-600"}`}>
           { (id < 10) ? `0${id}` : id }
         </h1>
-        <div className="h-full w-1 bg-gray-500" ref={lineRef}>
+        <div className="h-full w-1" ref={lineRef}>
           <div className="bg-purple-700" style={{height: lineHeight+"px"}}/>
         </div>
       </div>
-      <div className="p-5">
+      <div className="p-5 pt-0">
         {/* <p>{ compHeight } - { position.toFixed(0) } / { window.innerHeight/2 }</p> */}
         <h6 className="text-sm">{ tagline }</h6>
-        <h2 className="text-4xl font-bold">{ title }</h2>
-        <p>{ description }</p>
+        <h2 className="mb-3 text-5xl font-bold">{ title }</h2>
+        <p className="pb-20">{ description }</p>
       </div>
     </div>
   )
